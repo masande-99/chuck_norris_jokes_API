3d735b715dec4fc4ae1b9adc0f2e529de4f44307
masande <gontyelenimasande@gmail.com>
2021-02-08 15:34:38 +0200

    made a few changes to the main.py

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ def get_chuck_norris_jokes():
     api_url = "https://api.chucknorris.io/jokes/random"
     response = requests.get(api_url).json()
 
-    image_tag ="<img src="+response['icon_url']+"alt ='Chucks Image'>"
+    image_tag ="<img src="+response['icon_url']+"alt ='Chuck's Image'>"
 
     return "<strong>Random joke from chuck norris: </strong>" + response['value'] + image_tag
 
